e4879233a3767c8c05c60f03fd95d9f370c0a3c3
root <root@ip-172-31-11-248.ap-south-2.compute.internal>
2025-02-24 16:48:10 +0000

    Initial commit for Book Store App

---
+++ src/main/java/com/bookstore/BookStoreApp.java
@@ -0,0 +1,1 @@
+package com.bookstore; public class BookStoreApp { public static void main(String[] args) { System.out.println("Welcome to the Book Store"); } }
---
+++ src/test/java/com/bookstore/BookStoreAppTest.java
@@ -0,0 +1,1 @@
+package com.bookstore; import org.junit.jupiter.api.Test; import static org.junit.jupiter.api.Assertions.*; public class BookStoreAppTest { @Test public void testMain() { assertTrue(true); } }
